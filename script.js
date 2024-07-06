db81761d8f8b9c0c4bf9e49d5f5085c5b92bfd50
Maheswaran K <119476950+KMahe06@users.noreply.github.com>
2024-07-06 09:27:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ $(document).ready(function() {
     });
 
   //contact form to excel sheet
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbzUSaaX3XmlE5m9YLOHOBrRuCh2Ohv49N9bs4bew7xPd1qlgpvXtnudDs5Xhp3jF-Fx/exec';
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxBi0FS9tFgpwJAkU6GxY62jFgnIHFgHsTSdE6PhUjiz25vnP17rOEUhIyXTs5NKAiOOA/exec';
   const form = document.forms['submitToGoogleSheet']
   const msg = document.getElementById("msg")
 
